c7adaa17a8a1defa0c30981c248734b4eb1f70bb
Chris Svajlenka <chris@svajlenka.com>
2022-07-17 18:19:08 -0700

    fix(ui): re-adding dotenv config for ui

--- src/config.js
+++ src/config.js
@@ -1,9 +1,9 @@
-// TODO: figure out hwo to get github pages to work with env vars / parcel / type="module"
-// import {config} from "dotenv";
-// config({ path: ".env" });
+
+import {config} from "dotenv";
+config({ path: ".env" });
 
 window.config = {
-    ALGOLIA_APP_ID: "INO7S66H6Z",
-    ALGOLIA_API_KEY: "fbb845db59ed1a6a0f2b25e64347579d", // search-only (is committed to repo!)
-    ALGOLIA_INDEX_NAME: "bestbuy",
+    ALGOLIA_APP_ID:process.env.ALGOLIA_APP_ID,
+    ALGOLIA_API_KEY: process.env.ALGOLIA_API_KEY, // search-only (is committed to repo!)
+    ALGOLIA_INDEX_NAME: process.env.ALGOLIA_INDEX_NAME,
 }
